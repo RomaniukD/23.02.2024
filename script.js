60bc4b882648f69877f22331a4c1b5f8258932b0
RomaniukD <denromaniuk2008@gmail.com>
2024-06-02 00:37:08 +0300

    final1.0

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-.
+  
+ 
+ document.getElementById('solutions_button_mobilise').addEventListener('click', function() {
+    document.getElementById('content_section').innerHTML = `
+    
+                    <div style="display: inline-block; width: 50%; vertical-align: top;" >
+                    <h3 style="font-size: 2em; text-align: center;">Acquire and engage global customers with scalable incentives</h3>
+                    <h3>Engage and incentivize customers with highly motivating and cost-effective data bundles and vouchers. 
+                        Avoid the complexity of cash rewards with non-cash incentives, distributed in real time with full reporting to track conversion.</h3>
+                    </div>
+                    <img src="img/mobilise_section.png" style='height: 20em;' ></img>`
+});
+
+document.getElementById('solutions_button_monetise').addEventListener('click', function() {
+    document.getElementById('content_section').innerHTML = `
+    
+                    <div style="display: inline-block; width: 50%; vertical-align: top;" >
+                    <h3 style="font-size: 2em; text-align: center;">Lead & Sales Generation That Works</h3>
+                    <h3>Monetise is a global performance marketing network. We specialise in customer acquisition with more than 
+                    a decade of lead generation and media buying experience across the business-to-consumer space.</h3>
+                    </div>
+                    <img src="img/monetise.png" style='height: 20em;' ></img>
+        
+    `
+    
+})
+
+document.getElementById('solutions_button_retein').addEventListener('click', function() {
+    document.getElementById('content_section').innerHTML = `
+    
+                <div style="display: inline-block; width: 50%; vertical-align: top;" >
+                    <h3 style="font-size: 2em; text-align: center;"> Strategies to Use to Retain and Reward Your Customers</h3>
+                    <h3>We at Reward the World understand the critical role customer retention plays in the success of any business. 
+                    High retention rates are not just beneficial; they are a testament to a company’s strength in fostering customer loyalty.</h3>
+                    </div>
+                    <img src="img/motivate_reward.jpg" style='height: 20em;' ></img>
+        
+    `
+})
